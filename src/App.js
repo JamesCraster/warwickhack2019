f54d8845c8a29adad9a5551e1ea270b2079bf859
Will Russell <will@will-russell.com>
2019-02-23 16:32:48 +0000

    menu logo now has alt

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { Header, Menu} from "semantic-ui-react";
 import ChatBox from "./ChatBox";
 import send from "./audio/encoder";
 
-
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ textAlign: "center", height: "100%" }}>
         <Menu inverted fluid>
           <Menu.Item header>
-            <img src="logo-white.png" />
+            <img src="logo-white.png" alt="Logo" />
           </Menu.Item>
           <Menu.Item header>Talking-2.0</Menu.Item>
         </Menu>
