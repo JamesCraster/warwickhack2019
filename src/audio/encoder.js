2938f0662b1c3337da2f62e27f473e6114c4d2e3
JamesCraster <jamesvaughancraster@gmail.com>
2019-02-23 15:29:09 +0000

    works for single characters

--- src/audio/encoder.js
+++ src/audio/encoder.js
@@ -1,8 +1,8 @@
 var packet_delay = 1000; //ms
 var bit_pulse_delay = 500; //ms
 
-var freq_high = 2000; //hz
-var freq_low = 1000; //hz
+var freq_high = 3000; //hz
+var freq_low = 2000; //hz
 
 var word_length = 8;
 
@@ -15,24 +15,33 @@ function send(str) {
   var char_index = 0;
   audioContext.resume();
   generate_bit(0);
-  for (var i = 0; i < str.length - 1; i++) {
+  for (var i = 0; i < str.length; i++) {
     setTimeout(() => {
-      console.log(str.charCodeAt(char_index));
+      //console.log(str.charCodeAt(char_index));
       generate_packet(str.charCodeAt(char_index));
       char_index++;
-    }, packet_delay * i + bit_pulse_delay * (word_length + 2));
+    }, packet_delay * i);
   }
+  /*
+  console.log("change");
+  for (let x = 0; x < str.length; x++) {
+    console.log(packet_delay * x + bit_pulse_delay * (word_length + 2));
+  }*/
+  console.log(
+    (packet_delay + (word_length + 2) * bit_pulse_delay) * str.length,
+  );
   setTimeout(() => {
-    oscillator.disconnect();
-  }, packet_delay * (word_length + 2) + bit_pulse_delay * (word_length + 2));
+    //oscillator.disconnect();
+  }, (packet_delay + (word_length + 2) * bit_pulse_delay) * str.length);
 }
 
 function generate_packet(value) {
   //Convert to Binary Array
   var bin = [];
   for (var i = 0; i < word_length; i++) {
-    bin[i] = (value >> i) & 1;
+    bin[i] = (value >> (word_length - 1 - i)) & 1;
   }
+  console.log(bin);
   var byte_index = 0;
   //Transmit header
   generate_bit(1);
@@ -63,6 +72,7 @@ function generate_sine(freq) {
   oscillator.type = "sine";
   oscillator.frequency.setValueAtTime(freq, audioContext.currentTime);
   oscillator.connect(audioContext.destination);
+  //console.log(audioContext.currentTime);
 }
 
 export default send;
