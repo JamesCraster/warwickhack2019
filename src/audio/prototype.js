057b2615da77b7f000c78b11e33158aadcdccee4
Will Russell <will@will-russell.com>
2019-02-23 15:48:54 +0000

    Moved prototype to src

--- src/audio/prototype.js
+++ src/audio/prototype.js
@@ -1,19 +1,85 @@
 document.addEventListener("DOMContentLoaded", init);
 
-const high_freq_centre_bin = 34;
+const high_freq_centre_bin = 35;
 const low_freq_centre_bin = 23;
 const bin_averaging_width = 2;
 
+const packet_delay = 1000; //ms
+const bit_pulse_delay = 200; //ms
+
+const n_way_majority = 1;
+const payload_length = 8 * n_way_majority;
+
+var packet_data = [];
+var remaining_bits = 0;
+
+function callback(value) {
+  console.log(value + " = " + String.fromCharCode(value));
+}
+
+function decode_packet(data) {
+  console.log("raw packet: " + data);
+
+  var decoded = [];
+
+  for (var i = 0; i < payload_length; i += n_way_majority) {
+    var votes = data.slice(i, i + n_way_majority);
+    decoded.push(decode_n_way_majority(votes, n_way_majority));
+    console.log("find majority: " + votes + " = " + decoded[decoded.length - 1]);
+  }
+
+  console.log("decoded packet:" + decoded);
+
+  var value = 0;
+
+  for (var i = decoded.length; i > 0; i--) {
+    if (decoded[decoded.length - i]) {
+      value += Math.pow(2, i - 1);
+    }
+  }
+
+  callback(value);
+}
+
+function start_polling() {
+  start_bit_checker = setInterval(check_for_start_bit, 10);
+}
+
+function check_for_start_bit() {
+  analyser.getByteFrequencyData(fft);
+  if (is_high(200)) {                   // we have found the rising edge
+    clearInterval(start_bit_checker);    // stop checking for it
+    setTimeout(start_sampling, bit_pulse_delay * 1.5);  // start sampling the packet
+  }
+}
+
+function start_sampling() {
+  packet_data = [];
+  remaining_bits = payload_length;
+  bit_sampler = setInterval(sample, bit_pulse_delay);
+}
+
+function sample() {
+  if (remaining_bits > 0) {
+    remaining_bits--;
+    packet_data.push(sample_bit());
+  } else {
+    clearInterval(bit_sampler);
+    decode_packet(packet_data);
+    setTimeout(start_polling, packet_delay * 0.8);
+  }
+}
+
 function init() {
-  audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+  audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   analyser = audioCtx.createAnalyser();
 
   analyser.fftSize = 512;
   analyser.frequencyBinCount = 1024;
-  analyser.smoothingTimeConstant = 0;
+  analyser.smoothingTimeConstant = 0.2;
   bufferLength = analyser.frequencyBinCount;
-  dataArray = new Uint8Array(bufferLength);
-  analyser.getByteTimeDomainData(dataArray);
+  fft = new Uint8Array(bufferLength);
+  analyser.getByteTimeDomainData(fft);
 
   canvas = document.getElementById("screen");
   canvasCtx = canvas.getContext("2d");
@@ -21,7 +87,7 @@ function init() {
   high_output = document.getElementById("high");
   low_output = document.getElementById("low");
 
-  var handleSuccess = function(stream) {
+  var handleSuccess = function (stream) {
     mic_stream = audioCtx.createMediaStreamSource(stream);
     mic_stream.connect(analyser);
   };
@@ -29,14 +95,17 @@ function init() {
   navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
 }
 
-function get_frequency_amplitude(fft, bin, averaging_width) {
-  var amplitude = 0;
+function get_frequency_amplitude(bin, averaging_width) {
+  // analyser.getByteTimeDomainData(fft);
+  var sum = 0;
+  var samples = 0;
 
   for (var i = bin - averaging_width; i < bin + averaging_width; i++) {
-    amplitude += fft[i];
+    sum += fft[i];
+    samples++;
   }
 
-  return amplitude / (averaging_width * 2 + 1);
+  return sum / samples;
 }
 
 
@@ -44,7 +113,7 @@ function draw() {
 
   requestAnimationFrame(draw);
 
-  analyser.getByteFrequencyData(dataArray);
+  analyser.getByteFrequencyData(fft);
 
   canvasCtx.fillStyle = "rgb(200, 200, 200)";
   canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
@@ -59,7 +128,7 @@ function draw() {
 
   for (var i = 0; i < bufferLength; i++) {
 
-    var v = dataArray[i] / 256.0;
+    var v = fft[i] / 256.0;
     var y = canvas.height - (v * canvas.height);
 
     if (i === 0) {
@@ -74,8 +143,32 @@ function draw() {
   canvasCtx.lineTo(canvas.width, canvas.height / 2);
   canvasCtx.stroke();
 
-  high_output.innerText = get_frequency_amplitude(dataArray, high_freq_centre_bin, bin_averaging_width);
-  low_output.innerText = get_frequency_amplitude(dataArray, low_freq_centre_bin, bin_averaging_width);
+  high_output.innerText = get_frequency_amplitude(high_freq_centre_bin, bin_averaging_width);
+  low_output.innerText = get_frequency_amplitude(low_freq_centre_bin, bin_averaging_width);
 }
 
 draw();
+
+function sample_bit() {
+  var high_amplitude = get_frequency_amplitude(high_freq_centre_bin, bin_averaging_width);
+  var low_amplitude = get_frequency_amplitude(low_freq_centre_bin, bin_averaging_width);
+
+  return high_amplitude > low_amplitude;
+}
+
+function is_high(threshold) {
+  var high_amplitude = get_frequency_amplitude(high_freq_centre_bin, bin_averaging_width);
+  return high_amplitude > threshold;
+}
+
+function decode_n_way_majority(values, ways) {
+  var sum = 0;
+
+  for (var i = 0; i < values.length; i++) {
+    if (values[i]) {
+      sum++;
+    }
+  }
+
+  return sum >= ((ways + 1) / 2);
+}
