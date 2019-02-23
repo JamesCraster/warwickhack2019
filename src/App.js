209aba9bcad36ff45c242e0a87cef1ba2270a755
JamesCraster <jamesvaughancraster@gmail.com>
2019-02-23 13:53:56 +0000

    Logo displayed on webpage@

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import { Input, Header, Menu, Segment } from "semantic-ui-react";
 import ChatBox from "./ChatBox";
 import send from "./audio/encoder";
 
-
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ textAlign: "center", height: "100%" }}>
         <Menu inverted fluid>
           <Menu.Item header>
-            <img src="logo-full.png" />
+            <img src="logo-white.png" />
           </Menu.Item>
           <Menu.Item header>Talking-2.0</Menu.Item>
         </Menu>
         <Header as="h1">Talking-2.0</Header>
-        <ChatBox sendToEncoder={send}/>
+        <ChatBox sendToEncoder={send} />
       </div>
     );
   }
