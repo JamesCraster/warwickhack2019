2cd83476d599c61606aa632d7de29fb092dc03b8
Will Russell <will@will-russell.com>
2019-02-23 19:44:54 +0000

    Refactor

--- src/audio/encoder.js
+++ src/audio/encoder.js
@@ -19,7 +19,6 @@ function send(str) {
   setTimeout(() => {
     for (var i = 0; i < str.length; i++) {
       setTimeout(() => {
-        //console.log(str.charCodeAt(char_index));
         generate_packet(str.charCodeAt(char_index));
         char_index++;
       }, (packet_delay + (word_length * 3 + 3) * bit_pulse_delay) * i);
@@ -87,7 +86,6 @@ function generate_sine(freq) {
   oscillator.type = "sine";
   oscillator.frequency.setValueAtTime(freq, audioContext.currentTime);
   oscillator.connect(audioContext.destination);
-  //console.log(audioContext.currentTime);
 }
 
 export default send;
