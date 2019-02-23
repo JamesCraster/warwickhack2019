1d7eddbd64e45b224ea1898946d779c8b9717440
edwin <edwinclarke4@gmail.com>
2019-02-23 12:07:27 +0000

    Implemented core encoding functions. Needs testing!

---
+++ audio/encoder.js
@@ -0,0 +1,46 @@
+var packet_delay = 1000; //ms
+var bit_pulse_delay = 500; //ms
+
+var freq_high = 2000; //hz
+var freq_low = 1000; //hz
+
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+var oscillator = audioContext.createOscillator();
+
+function generate_packet(value) {
+  //Convert to Binary Array
+  var bin = [];
+  for (var i = 0; i < 8; i++)
+  {
+    b[i] = (value >> i) & 1;
+  }
+
+  //Transmit header
+  generate_bit(1);
+
+setTimeout(function(){
+  //Transmit payload
+  for (var i = 0; i < 8; i++) {
+    setTimeout(function() {
+      generate_bit(bin[i]);
+    },i*bit_pulse_delay);
+  }
+  stop_oscillator();
+  },bit_pulse_delay);
+}
+
+function generate_bit(bit) {
+  generate_sine(value == 1 ? freq_high : freq_low);
+}
+
+function generate_sine(freq) {
+  oscillator.type="sine";
+  oscillator.frequency.setValueAtTime(freq, audioContext.currentTime);
+  oscillator.connect(audioContext.destination);
+  oscillator.start();
+}
+
+function stop_oscillator() {
+  oscillator.stop();
+}
