c8a312a5ab5f15533bb816881fffafabd7f4c1a2
JamesCraster <jamesvaughancraster@gmail.com>
2019-02-23 17:18:17 +0000

    Updates

---
+++ precache-manifest.3705f1541f9ec863c05a6f1f33e605e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73ab766da7848e5470d8",
+    "url": "/warwickhack2019/static/js/runtime~main.73ab766d.js"
+  },
+  {
+    "revision": "fcb26e0b7f40d5599b2d",
+    "url": "/warwickhack2019/static/js/main.fcb26e0b.chunk.js"
+  },
+  {
+    "revision": "3a7aa3dd84f6c6c34335",
+    "url": "/warwickhack2019/static/js/2.3a7aa3dd.chunk.js"
+  },
+  {
+    "revision": "fcb26e0b7f40d5599b2d",
+    "url": "/warwickhack2019/static/css/main.83db9184.chunk.css"
+  },
+  {
+    "revision": "65dba5cf45f37744152e76f579b50ae9",
+    "url": "/warwickhack2019/index.html"
+  }
+];
