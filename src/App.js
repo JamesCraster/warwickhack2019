069a9a4b42e766b06f7ba36651eb03b1709bc5d0
JamesCraster <jamesvaughancraster@gmail.com>
2019-02-23 11:08:56 +0000

    started markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Input, Header, Menu } from "semantic-ui-react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Menu inverted fluid>
+          <Menu.Item header>Talking-2.0</Menu.Item>
+        </Menu>
+        <Header as="h1">Talking-2.0</Header>
+        <Input action="Submit" placeholder="Enter text..." />
       </div>
     );
   }
